a4ab265ee8a58cda5fb0be782dd8d8399632a9b2
MageLink <mage.link.zhezard@gmail.com>
2023-01-26 22:12:14 -0300

    Terminé la primera. Aunque falta testear al menos 1 objeto

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -1,6 +1,18 @@
-const request = require("supertest");
+const supertest = require("supertest");
+const { number } = require("yargs");
 const server = require("../index");
 
 describe("Operaciones CRUD de cafes", () => {
-
+  it("Test get path for 200 code", async () => {
+    const response = await supertest(server).get("/cafes").send();
+    const status = response.statusCode;
+    const { body: cafes } = response;
+    console.log("cafes", cafes);
+    expect(status).toBe(200);
+    //expect(cafes).toEqual(expect.arrayContaining([expect.objectContaining({ id: 1 })]));
+    //expect(cafes).toEqual(expect.arrayContaining([expect.toMatchSnapshot({ id: expect.any(number) })]));
+    //expect(cafes).toContain(expect.arrayContaining([expect.objectContaining({ id: expect.any(Number) })]));
+    //expect(cafes).toEqual(expect.arrayContaining([expect.toBeInstanceOf(Object)]));
+    expect(cafes).toBeInstanceOf(Array);
+  });
 });
