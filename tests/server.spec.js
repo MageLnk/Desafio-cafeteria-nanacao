d6f26bb85325e2b083845dc7254f1f157db041e1
MageLink <mage.link.zhezard@gmail.com>
2023-01-27 00:18:48 -0300

    Second it, the 404 in case wrong id, is completed

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -1,18 +1,25 @@
 const supertest = require("supertest");
-const { number } = require("yargs");
 const server = require("../index");
 
 describe("Operaciones CRUD de cafes", () => {
-  it("Test get path for 200 code", async () => {
+  it("Test get -/cafes- path for 200 code", async () => {
     const response = await supertest(server).get("/cafes").send();
     const status = response.statusCode;
     const { body: cafes } = response;
-    console.log("cafes", cafes);
+    //console.log("cafes", cafes);
     expect(status).toBe(200);
     //expect(cafes).toEqual(expect.arrayContaining([expect.objectContaining({ id: 1 })]));
     //expect(cafes).toEqual(expect.arrayContaining([expect.toMatchSnapshot({ id: expect.any(number) })]));
     //expect(cafes).toContain(expect.arrayContaining([expect.objectContaining({ id: expect.any(Number) })]));
     //expect(cafes).toEqual(expect.arrayContaining([expect.toBeInstanceOf(Object)]));
     expect(cafes).toBeInstanceOf(Array);
+  });
+
+  it("Test delete -/cafes/:id- path for 404 code in case for wrong id", async () => {
+    const jwt = "jwtTest";
+    const idTestingForDelete = 6;
+    const response = await supertest(server).delete(`/cafes/${idTestingForDelete}`).set("Authorization", jwt).send();
+    const status = response.statusCode;
+    expect(status).toBe(404);
   });
 });
